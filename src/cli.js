fe863f407975eea1286e6ea85c2e6a70a4ff7c86
Ilya Trushin <ilya.trushin@mail.ru>
2022-06-30 03:00:14 +0400

    Stage four

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const getName = () => {
-  console.log('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games!'); // eslint-disable-line no-console
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}!`); // eslint-disable-line no-console
 };
 
 export default getName;
