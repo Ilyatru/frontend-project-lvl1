44cd1674c89a8596327711a15fccfb15344cbcf6
Ilya Trushin <ilya.trushin@mail.ru>
2022-07-02 03:17:58 +0400

    Make three round of brain-Even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,8 +2,8 @@
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!'); // eslint-disable-line no-console
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`); // eslint-disable-line no-console
+const playerName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${playerName}!`); // eslint-disable-line no-console
 console.log('Answer "yes" if the number is even, otherwise answer "no".'); // eslint-disable-line no-console
 
 const getRandomInRange = (min, max) => {
@@ -11,18 +11,21 @@ const getRandomInRange = (min, max) => {
 };
 
 const evenGame = () => {
-  const number = getRandomInRange(0, 100);
-  console.log('Question:', number); // eslint-disable-line no-console
-  const answer = readlineSync.question('Your answer: ');
-  const checkYes = (number % 2 === 0) && (answer === 'yes');
-  const checkNo = (number % 2 !== 0) && (answer === 'no');
-  if ((checkYes === true) || (checkNo === true)) {
-    console.log('Correct!'); // eslint-disable-line no-console
-  } else {
-    const correctAnswer = (answer === 'yes' ? 'no' : 'yes');
-    console.log(`"${answer}" is wrong answer ;(. Correct answer was '${correctAnswer}'.`); // eslint-disable-line no-console
-    console.log(`Let's try again, ${name}!`); // eslint-disable-line no-console
-  }
+  for (let i = 0; i < 3; i += 1) {
+    const number = getRandomInRange(0, 100);
+    console.log('Question:', number); // eslint-disable-line no-console
+    const answer = readlineSync.question('Your answer: ');
+    const checkYes = (number % 2 === 0) && (answer === 'yes');
+    const checkNo = (number % 2 !== 0) && (answer === 'no');
+    if ((checkYes === true) || (checkNo === true)) {
+      console.log('Correct!'); // eslint-disable-line no-console
+    } else {
+      const correctAnswer = (answer === 'yes' ? 'no' : 'yes');
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was '${correctAnswer}'.`); // eslint-disable-line no-console
+      console.log(`Let's try again, ${playerName}!`); // eslint-disable-line no-console
+      return;
+    }
+  } console.log(`Congratulations, ${playerName}!`); // eslint-disable-line no-console
 };
 
 evenGame();
