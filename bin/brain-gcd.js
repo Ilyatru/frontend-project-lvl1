2530f48d815abe559392b5ec7918223366d19199
Ilya Trushin <ilya.trushin@mail.ru>
2022-07-22 22:43:50 +0400

    Added gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import gcdGame from '../games/logic-gcd.js';
+
+gcdGame();
---
+++ games/logic-gcd.js
@@ -0,0 +1,22 @@
+import readlineSync from 'readline-sync';
+import { getRandomInRange, playerName } from '../src/index.js';
+
+const gcdGame = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const number = getRandomInRange(0, 100);
+    console.log('Question:', number); // eslint-disable-line no-console
+    const answer = readlineSync.question('Your answer: ');
+    const checkYes = (number % 2 === 0) && (answer === 'yes');
+    const checkNo = (number % 2 !== 0) && (answer === 'no');
+    if ((checkYes === true) || (checkNo === true)) {
+      console.log('Correct!'); // eslint-disable-line no-console
+    } else {
+      const correctAnswer = (answer === 'yes' ? 'no' : 'yes');
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was '${correctAnswer}'.`); // eslint-disable-line no-console
+      console.log(`Let's try again, ${playerName}!`); // eslint-disable-line no-console
+      return;
+    }
+  } console.log(`Congratulations, ${playerName}!`); // eslint-disable-line no-console
+};
+
+export default gcdGame;
