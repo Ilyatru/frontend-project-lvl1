6fb2e4313c75a7aad01fce2bdf92636a7d3935cf
Ilya Trushin <ilya.trushin@mail.ru>
2022-07-29 15:46:37 +0400

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-export const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+export const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 console.log('Welcome to the Brain Games!');
 export const playerName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${playerName}!`);
