2bc87099e5b379185be6c79d15de3f1734c226bc
Ilya Trushin <ilya.trushin@mail.ru>
2022-08-28 23:49:54 +0400

    Changes after third review in progression-game

--- src/games/logic-progression.js
+++ src/games/logic-progression.js
@@ -1,32 +1,24 @@
-import readlineSync from 'readline-sync';
-import { getRandomInRange, playerName } from '../index.js';
+import makeCommonActions from '../index.js';
+import getRandomInRange from '../helpers.js';
 
-const progressionGame = () => {
-  console.log('What number is missing in the progression?');
-  for (let i = 0; i < 3; i += 1) {
-    const progression = [];
-    progression[0] = getRandomInRange(0, 10);
-    const difference = getRandomInRange(0, 10);
-    const missingElementIndex = getRandomInRange(0, 9);
+const gameDescription = 'What number is missing in the progression?';
 
-    for (let y = 0; y < 9; y += 1) {
-      const progressionElement = progression[y] + difference;
-      progression.push(progressionElement);
-    }
-    const correctAnswer = progression[missingElementIndex];
-    progression[missingElementIndex] = '..';
-    const strProgression = progression.join(' ');
-    console.log('Question:', strProgression);
-    const answer = readlineSync.question('Your answer: ');
+const makeRound = () => {
+  const progression = [];
+  progression[0] = getRandomInRange(0, 10);
+  const difference = getRandomInRange(0, 10);
+  const missingElementIndex = getRandomInRange(0, 9);
 
-    if (+answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
-  } console.log(`Congratulations, ${playerName}!`);
+  for (let y = 0; y < 9; y += 1) {
+    const progressionElement = progression[y] + difference;
+    progression.push(progressionElement);
+  }
+
+  const correctAnswer = progression[missingElementIndex];
+  progression[missingElementIndex] = '..';
+  const question = progression.join(' ');
+
+  return [question, correctAnswer.toString()];
 };
 
-export default progressionGame;
+export default () => makeCommonActions(gameDescription, makeRound);
