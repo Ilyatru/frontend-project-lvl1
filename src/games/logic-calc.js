37d69fb34199827f22d289e9afe900938717267c
Ilya Trushin <ilya.trushin@mail.ru>
2022-08-28 23:34:13 +0400

    Changes after third review in calc-game

--- src/games/logic-calc.js
+++ src/games/logic-calc.js
@@ -1,37 +1,27 @@
-import readlineSync from 'readline-sync';
-import { getRandomInRange, playerName } from '../index.js';
+import makeCommonActions from '../index.js';
+import getRandomInRange from '../helpers.js';
 
-const calcGame = () => {
-  console.log('What is the result of the expression?');
-  for (let i = 0; i < 3; i += 1) {
-    const firstNumber = getRandomInRange(0, 100);
-    const secondNumber = getRandomInRange(0, 100);
-    const mathSymbolNumber = getRandomInRange(0, 2);
-    let mathSymbol;
-    let correctAnswer;
+const gameDescription = 'What is the result of the expression?';
 
-    if (mathSymbolNumber === 0) {
-      mathSymbol = '+';
-      correctAnswer = +firstNumber + +secondNumber;
-    } else if (mathSymbolNumber === 1) {
-      mathSymbol = '-';
-      correctAnswer = +firstNumber - +secondNumber;
-    } else {
-      mathSymbol = '*';
-      correctAnswer = +firstNumber * +secondNumber;
-    }
+const makeRound = () => {
+  const firstNumber = getRandomInRange(0, 100);
+  const secondNumber = getRandomInRange(0, 100);
+  const mathSymbolNumber = getRandomInRange(0, 2);
+  let mathSymbol;
+  let correctAnswer;
+  if (mathSymbolNumber === 0) {
+    mathSymbol = '+';
+    correctAnswer = +firstNumber + +secondNumber;
+  } else if (mathSymbolNumber === 1) {
+    mathSymbol = '-';
+    correctAnswer = +firstNumber - +secondNumber;
+  } else {
+    mathSymbol = '*';
+    correctAnswer = +firstNumber * +secondNumber;
+  }
+  const question = `${firstNumber}${mathSymbol}${secondNumber}`;
 
-    console.log('Question:', firstNumber, mathSymbol, secondNumber);
-    const answer = readlineSync.question('Your answer: ');
-
-    if (+answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
-  } console.log(`Congratulations, ${playerName}!`);
+  return [question, correctAnswer.toString()];
 };
 
-export default calcGame;
+export default () => makeCommonActions(gameDescription, makeRound);
