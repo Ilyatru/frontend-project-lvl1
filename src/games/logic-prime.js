c0a52cf8a63d6d2f71aebcf630ca18e751928ad9
Ilya Trushin <ilya.trushin@mail.ru>
2022-08-29 00:03:20 +0400

    Changes after third review in prime-game

--- src/games/logic-prime.js
+++ src/games/logic-prime.js
@@ -1,34 +1,24 @@
-import readlineSync from 'readline-sync';
-import { getRandomInRange, playerName } from '../index.js';
+import makeCommonActions from '../index.js';
+import getRandomInRange from '../helpers.js';
 
-const primeGame = () => {
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const divisorsArr = [];
-    const number = getRandomInRange(2, 100);
-    let correctAnswer;
-
-    for (let y = 3; y < number; y += 1) {
-      if (number % y === 0) {
-        divisorsArr.push(y);
-      }
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const isPrime = (someNumber) => {
+  const divisorsArr = [];
+  for (let y = 3; y < someNumber; y += 1) {
+    if (someNumber % y === 0) {
+      divisorsArr.push(y);
     }
+  }
 
-    if (divisorsArr.length > 0) {
-      correctAnswer = 'no';
-    } else correctAnswer = 'yes';
-
-    console.log('Question:', number);
-    const answer = readlineSync.question('Your answer: ');
-
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
-  } console.log(`Congratulations, ${playerName}!`);
+  if (divisorsArr.length > 0) {
+    return false;
+  } return true;
 };
 
-export default primeGame;
+const makeRound = () => {
+  const question = getRandomInRange(2, 100);
+  const correctAnswer = (isPrime(question)) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export default () => makeCommonActions(gameDescription, makeRound);
