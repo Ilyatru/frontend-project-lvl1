26ee2a8167f2505e9584840fe8fc30719260dc5d
Ilya Trushin <ilya.trushin@mail.ru>
2022-09-01 21:58:18 +0400

    Added generateProgression function

--- src/games/logic-progression.js
+++ src/games/logic-progression.js
@@ -2,21 +2,27 @@ import generateGame from '../index.js';
 import getRandomInRange from '../helpers.js';
 
 const gameDescription = 'What number is missing in the progression?';
-
-const generateRound = () => {
+const generateProgression = (firstNumber, commonDifference, missingElementIndex) => {
   const progression = [];
-  progression[0] = getRandomInRange(0, 10);
-  const difference = getRandomInRange(0, 10);
-  const missingElementIndex = getRandomInRange(0, 9);
+  progression[0] = firstNumber;
   const progressionLength = 10;
 
-  for (let y = 0; y < (progressionLength - 1); y += 1) {
-    const progressionElement = progression[y] + difference;
+  for (let i = 0; i < (progressionLength - 1); i += 1) {
+    const progressionElement = progression[i] + commonDifference;
     progression.push(progressionElement);
   }
 
-  const correctAnswer = progression[missingElementIndex];
   progression[missingElementIndex] = '..';
+
+  return progression;
+};
+
+const generateRound = () => {
+  const firstNumber = getRandomInRange(0, 10);
+  const difference = getRandomInRange(0, 10);
+  const missingElementIndex = getRandomInRange(0, 9);
+  const progression = generateProgression(firstNumber, difference, missingElementIndex);
+  const correctAnswer = +progression[missingElementIndex + 1] - difference;
   const question = progression.join(' ');
 
   return [question, correctAnswer.toString()];
